eb4acc68f91eb1d1dac0d9619c8e62ebc7239256
Prasann199 <95702193+Prasann199@users.noreply.github.com>
2025-07-26 22:56:34 +0530

    Update Navbar.jsx

--- src/assets/Navbar/Navbar.jsx
+++ src/assets/Navbar/Navbar.jsx
@@ -165,7 +165,7 @@ const Navbar = () => {
     <div className="relative">
       <nav className="bg-[#0c1a35] text-white fixed top-0 w-full z-50 shadow-md flex justify-center" style={{padding:"0px 10px"}}>
         <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between h-[13vh] w-[98%]">
-          <div className="text-2xl font-bold font-sans tracking-wide">
+          <div className="text-2xl font-bold font-sans tracking-wider">
             Book<span className="text-[#ff3147] italic">My</span>Bed
           </div>
           <div className="flex items-center gap-4">
